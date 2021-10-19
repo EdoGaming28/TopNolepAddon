1952462dc6ff2bd8d04527858f7f36e5a86b4cd2
Edo <75941214+EdoGaming28@users.noreply.github.com>
2021-10-19 18:32:07 +0700

    Create TopNolepAddon.php

---
+++ TopNolepAddon.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types = 1);
+
+/**
+ * @name TopNolepAddpn
+ * @version 1.0.0
+ * @main JackMD\ScoreHud\Addons\TopNolepAddon
+ * @depend TopNolep
+ */
+namespace JackMD\ScoreHud\Addons {
+
+    use JackMD\ScoreHud\addon\AddonBase;
+    use pocketmine\Player;
+    use TopNolep\EdoGaming\Main;
+
+    class TopNolepAddon extends AddonBase{
+
+        /** @var TopNolep */
+        private $topnolep;
+
+        public function onEnable() : void{
+            $this->topnolep = $this->getServer()->getPluginManager()->getPlugin("TopNolep");
+        }
+
+        /**
+         * @param Player $player
+         * @return array
+         */
+        public function getProcessedTags(Player $player): array{
+            return [
+                "{nolep_time}"   => $this->getTotal($player->getName())
+                ];
+        }
+
+        public function getTotal($pn){
+            $pn = strtolower($pn);
+            return isset(Main::$times[$pn]) ? $this->onlinetime->getNolepTime($pn) : "null";
+        }
+    }
+}
